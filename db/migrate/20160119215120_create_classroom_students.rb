23d2b8349e195aa7039c6aa8f240aa80afc7f905
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-05 18:25:19 +0000

    Automatically backed up by Learn

--- db/migrate/20160119215120_create_classroom_students.rb
+++ db/migrate/20160119215120_create_classroom_students.rb
@@ -1,4 +1,4 @@
-class CreateClassroomStudents < ActiveRecord::Migration
+class CreateClassroomStudents < ActiveRecord::Migration[5.2.2]
   def change
     create_table :classroom_students do |t|
       t.integer :classroom_id
